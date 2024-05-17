41ab13b8cad13849c2affa8f5605cf66ffa8da5d
Arun <sarkar89@gmail.com>
2024-05-17 22:46:18 +0530

    Goals Oulined in main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+
+# The Goal of this program is:
+# 1. to convert and compress videos
+# 2. convert to libx265 for compression
+# 3. covert to .mp4, as many apps play it directly sans downloading into the device
+# 4. If subtitle, hard-sub it
+# 5. In future, develop a UI front-end
+
+def main():
+    print("Hello Program")
+# End of 'def main():'
+
+if __name__ == "__main__":
+    main()
+# End of 'if __name__ == "__main__":'
